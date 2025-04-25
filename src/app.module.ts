ef3175c1d106b9754dfe0dbf457e4d7723f919bd
Felipe <felipefl200@gmail.com>
2025-04-25 18:38:40 -0300

    Add User resources and Format with Prettier

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { EnvConfigModule } from './shared/infrastruture/env-config/env-config.module';
+import { EnvConfigModule } from './shared/infrastruture/env-config/env-config.module'
+import { UsersModule } from './users/infrastructure/users.module'
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), EnvConfigModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    EnvConfigModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
