2e1c585e67383af8638638e803c3781f647483d7
Felipe França <felipefl200@gmail.com>
2026-03-24 12:10:31 -0300

    feat: add UserDataBuilder helper and refactor user entity unit tests to utilize it.

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -1,20 +1,11 @@
-import { randomUUID } from 'node:crypto'
 import { UserEntity, UserProps } from '../../user.entity'
-import { fakerPT_BR as faker } from '@faker-js/faker'
+import { UserDataBuilder } from '../../testing/helpers/user-data-builder'
 
 describe('UserEntity unit tests', () => {
   let props: UserProps
   let sut: UserEntity
   beforeEach(() => {
-    const firstName = faker.person.firstName()
-    const lastName = faker.person.lastName()
-    const email = `${firstName}.${lastName}@${faker.internet.domainName()}`
-    props = {
-      id: randomUUID(),
-      name: `${firstName} ${lastName}`,
-      email: email.toLowerCase(),
-      password: faker.internet.password(),
-    }
+    props = UserDataBuilder({})
     sut = new UserEntity(props)
   })
 
---
+++ src/users/domain/entities/testing/helpers/user-data-builder.ts
@@ -0,0 +1,25 @@
+import { fakerPT_BR as faker } from '@faker-js/faker'
+import { UserProps } from '../../user.entity'
+
+type Props = {
+  id?: string
+  name?: string
+  email?: string
+  password?: string
+  createdAt?: Date
+  updatedAt?: Date
+}
+
+export function UserDataBuilder(props: Props): UserProps {
+  const firstName = faker.person.firstName()
+  const lastName = faker.person.lastName()
+  const email = `${firstName}.${lastName}@${faker.internet.domainName()}`
+  return {
+    id: props.id ?? faker.string.uuid(),
+    name: props.name ?? `${firstName} ${lastName}`,
+    email: props.email ?? email,
+    password: props.password ?? faker.internet.password(),
+    createdAt: props.createdAt ?? new Date(),
+    updatedAt: props.updatedAt ?? new Date(),
+  }
+}
