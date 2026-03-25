3c87b351654db5cb1990248857ab105bfbf0078d
Felipe França <felipefl200@gmail.com>
2026-03-25 13:37:27 -0300

    test: add validation tests for invalid user name type and various invalid email values

--- src/users/domain/entities/__tests__/integration/user.entity-int.spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity-int.spec.ts
@@ -19,9 +19,35 @@ describe('UserEntity Integration Tests', () => {
 
       props = {
         ...UserDataBuilder({}),
+        name: 123123123 as any,
+      }
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+
+      props = {
+        ...UserDataBuilder({}),
         name: 'a'.repeat(256),
       }
       expect(() => new UserEntity(props)).toThrow(EntityValidationError)
     })
+  })
+
+  it('should throw an error when an invalid email is provided', () => {
+    let props: UserProps = {
+      ...UserDataBuilder({}),
+      email: null as any,
+    }
+    expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+
+    props = {
+      ...UserDataBuilder({}),
+      email: '',
+    }
+    expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+
+    props = {
+      ...UserDataBuilder({}),
+      email: 'a'.repeat(11),
+    }
+    expect(() => new UserEntity(props)).toThrow(EntityValidationError)
   })
 })
